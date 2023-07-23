fe246fa79408ce285a77bc2a437d806aaaffd7b3
lucien del mar <luciendelmar@luciens-MacBook-Air.local>
2023-07-23 14:10:20 +0200

    add import feature

--- fonts.js
+++ fonts.js
@@ -33,10 +33,15 @@ async function fetchGoogleFonts() {
   const data = await response.json();
   return data;
 }
-const fetchedData = await fetchGoogleFonts();
-const filteredData = fetchedData.items.map(font => {
-  return {family: font.family, category: font.category}
-});
-KNOWN_FONTS = filteredData;
+
+try {
+  const fetchedData = await fetchGoogleFonts();
+  const filteredData = fetchedData.items.map(font => {
+    return {family: font.family, category: font.category}
+  });
+  KNOWN_FONTS = filteredData;
+} catch (error) {
+  alert(`I'm sorry, the following error occured: ${error}. Please try again.`)
+}
 
 export default KNOWN_FONTS;
--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 import KNOWN_FONTS from "./fonts.js";
 
-const select = document.querySelector("select");
-const defaultOption = document.createElement("option");
-select.appendChild(defaultOption);
-
-// CREATE SELECT OPTIONS WITH FONT DATA
+// CREATE DATALIST OPTIONS AND FONTS ARRAY
+const datalist = document.querySelector(".my-datalist");
+const fontsArray = [];
 KNOWN_FONTS.map((font) => {
   const option = document.createElement("option");
-  option.innerText = font.family;
-  option.value = `${font.family}, ${font.category}`;
-  select.appendChild(option);
+  option.value = font.family;
+  datalist.appendChild(option);
+  fontsArray.push([font.family, font.category]);
 });
 
 // HANDLE SELECT CHANGE
+const input = document.querySelector("input");
 const changingText = document.querySelector(".changing-text");
-select.addEventListener("change", () => {
-  changingText.style.fontFamily = select.value;
+const style = document.querySelector("style");
+input.addEventListener("input", (event) => {
+  fontsArray.map((fontArray) => {
+    if (fontArray[0] === event.target.value) {
+      changingText.style.fontFamily = fontArray;
+      event.target.value = "";
+      const importRule = `@import url("https://fonts.googleapis.com/css2?family=${fontArray[0]}&family=Arial&display=swap")`;
+      if (!style.innerText.includes(importRule)) 
+      style.appendChild(document.createTextNode(importRule));
+    }
+  });
 });
